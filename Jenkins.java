12efb685a06ee27947d841ac0e5326e5c2ca287b
Jogaraodandu <96335777+Jogaraodandu@users.noreply.github.com>
2022-03-03 17:58:17 +0530

    Update Jenkins.java

--- Jenkins.java
+++ Jenkins.java
@@ -2,5 +2,7 @@ public class Jenkins{
  public ststic void main(String args[]){
  System.out.println("Hello Jenkins");
    System.out.println("Good evening");
+   System.out.println("Good evening");
+   System.out.println("Good evening");
  }
  }
